22875aa12d5820ae952832fb679351f0a9f9a81a
gerol-r <gerol.revills@gmail.com>
2025-04-01 14:58:11 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 
 import './App.css'
+import Bookshelf from './components/Bookshelf/Bookshelf.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  
 
   return (
     <>
-<h1>sup yall</h1>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
     </>
+
   )
 }
 
